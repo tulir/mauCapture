90e25fd4eb341b2fe76e9ba149b8ea77a329ae38
Tulir293 <tulir293@maunium.net>
2016-01-20 17:45:21 +0200

    Bump version to Beta 4

--- src/net/maunium/maucapture2/MauCapture.java
+++ src/net/maunium/maucapture2/MauCapture.java
@@ -57,7 +57,7 @@ public class MauCapture {
 	private Random r = new Random(System.nanoTime());
 	public static final Font lato = createLato();
 	public static final File config = new File(new File(System.getProperty("user.home")), ".maucapture.json");
-	public static final String version = "2.0", versionFull = "2.0.0_B3";
+	public static final String version = "2.0", versionFull = "2.0.0_B4";
 	private JFrame frame;
 	private JButton capture, preferences, uploadMIS, uploadImgur, color;
 	private JToggleButton arrow, rectangle, circle, pencil, text, erase, crop;
